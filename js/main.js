41c8590188cd8290db3d2a4467605843136d96e3
AleajandraRuizP <alejandraruizpimentel@gmail.com>
2022-04-26 13:14:52 -0400

    dos

--- js/main.js
+++ js/main.js
@@ -3,3 +3,10 @@
 var edad= prompt("Cual es tu edad?");
 var calculodias= edad*365
 document.write("Tú edad es " + edad + " y tiene "+ calculodias+ " dias viviendo.");
+
+
+
+var edadMaxima= 90;
+var takisXsemana= ;
+var precioTakis= 1700;
+
