3c599f781190af56cfb3ddfac1a87d80b1dedea7
AleajandraRuizP <alejandraruizpimentel@gmail.com>
2022-04-26 14:31:04 -0400

    ahora si ultimo

--- js/main.js
+++ js/main.js
@@ -10,13 +10,13 @@ var edadMaxima= Number(prompt("¿Hasta que edad crees que vivirás?"));
 var nombreSnack= prompt("¿Como se llama tu snack favorio?")
 var snackXsemana= Number(prompt("¿Qué cantidad de "+ nombreSnack + " consumes por semana? Indicar número de paquetes terminados :P"));
 var snackXanho= (snackXsemana*4)*12;
-var precioSnack=Number(prompt("¿Cuanto cuesta tu snack favorito?"));
+var precioSnack=Number(prompt("¿Cuánto cuesta tu snack favorito?"));
 
 var snackXvida= (edadMaxima-edadActual) * snackXanho;
 
 var cuantoXsnack= precioSnack*snackXvida;
  document.write(" Necesitarás " +  snackXvida+ " " + nombreSnack +" para que te alcance hasta los "+ edadMaxima + "<br>");
- document.write("Necesitarás "+ cuantoXsnack + "CLP para comprarlos ");
+ document.write("Necesitarás "+ cuantoXsnack + "CLP para comprarlos. ");
 
 
 
